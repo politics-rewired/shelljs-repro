9183f6c190e00d3d8a616211e8b1af245c4caa21
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2021-05-29 11:34:33 -0400

    feat: complete repro and mitigation

--- repro.ts
+++ repro.ts
@@ -2,29 +2,46 @@ import * as shell from "shelljs";
 
 const REPO = "politics-rewired/shelljs-repro";
 
-const cloneRepo = () => {
+const cloneRepo = (cd: boolean) => {
   // Shallow clone the manifests
   const rand = Math.floor(Math.random() * 1000);
-  const localPath = `/tmp/shelljs-repro-${new Date().getTime()}-${rand}`;
-  const repoRemoteUri = `git@github.com:${REPO}.git`;
+  const tmpDir = shell.tempdir() as any;
+  const localPath = `${tmpDir}/shelljs-repro-${new Date().getTime()}-${rand}`;
   shell.set("-e");
-  shell.exec(`git clone --depth 1 ${repoRemoteUri} ${localPath}`);
+  const repoRemoteUri = `git@github.com:${REPO}.git`;
+  const cloneCommand = `git clone --depth 1 ${repoRemoteUri} ${localPath}`;
+  console.log(`cmd: ${cloneCommand}`);
+  shell.exec(cloneCommand);
   shell.cd(localPath);
 
   // Make changes to the repo
   shell.echo("Do stuff in the repo").to("tmp.txt");
 
+  // The key line!
+  if (cd) shell.cd(tmpDir);
+
   // Clean up
   shell.rm("-rf", localPath);
 };
 
-try {
-  cloneRepo();
-} catch (err) {
-  console.error(`error in attempt 0`, err);
-}
-try {
-  cloneRepo();
-} catch (err) {
-  console.error(`error in attempt 1`, err);
-}
+const attempt = (cd: boolean) => {
+  try {
+    cloneRepo(cd);
+  } catch (err) {
+    console.error(`error in cd:${cd} attempt 0`, err);
+  }
+  try {
+    cloneRepo(cd);
+  } catch (err) {
+    console.error(`error in cd:${cd} attempt 1`, err);
+  }
+};
+
+// Changing directory before removing works for both attempts
+attempt(true);
+
+// Changing directory works for first, but fails for second
+attempt(false);
+
+// All subsequent exec's fail with same error
+attempt(true);
